ff8053edcc6eec31035e667ed91fb59ff506b23a
svm43 <svm43@tpu.ru>
2026-02-14 07:44:31 +0700

    добавлена навигация между главной и личным кабинетом

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import {Routes, Route} from 'react-router'
 import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import TabSelector from './components/TableSelector/TabSelector';
-import PeriodSelector from './components/PeriodSelector/PerSelector';
 import Stats from './components/Stats/Stats';
 import Table from './components/Table/Table';
 
 const App = () => {
   const [activeTab, setActiveTab] = useState('personal');
-  const [currentPeriod, setCurrentPeriod] = useState({ type: 'semester', value: '2025-autumn', label: 'Осень 2025' });
+  // const [currentPeriod, setCurrentPeriod] = useState({ type: 'semester', value: '2025-autumn', label: 'Осень 2025' });
 
   // const handlePeriodChange = (type, value, label) => {
   //   setCurrentPeriod({ type, value, label });
@@ -18,13 +17,12 @@ const App = () => {
     <div className="app">
       <Header />
       <TabSelector activeTab={activeTab} onTabClick={setActiveTab} />
-      {/* <PeriodSelector onPeriodChange={handlePeriodChange} /> */}
-      {/* {activeTab === 'personal' && (
-        <StatsCards position="#15" hours="48" avgScore="4.8" />
-      )} */}
+      {activeTab === 'personal' && (
+        <Stats position="#15" hours="48" avgScore="4.8" />
+      )}
       <Table 
         activeTab={activeTab}
-        period={currentPeriod}
+        // period={currentPeriod}
       />
     </div>
   );
