97d6fdc69a0952b38641fbf50c44a53ab1be0d3c
svm43 <svm43@tpu.ru>
2026-02-14 08:55:36 +0700

    пробую настроить деплой

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,17 @@
-import { Route, Routes } from 'react-router'
-import {BrowserRouter} from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Profile from './components/Profile/Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/untitled-folder">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
